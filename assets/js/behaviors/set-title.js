90658285e3627603ed53a8849ec567a2131b8f54
Diomidis Spinellis <dds@aueb.gr>
2018-03-19 18:02:16 +0000

    Display and hyperlink man page title

---
+++ assets/js/behaviors/set-title.js
@@ -0,0 +1,23 @@
+// -------------------------------------------
+//   Set title
+// -------------------------------------------
+
+Grapse.Behaviors.SetTitle = Essential.Behavior.extend({
+  priority: 1,
+
+  init: function() {
+    var title = "<a href='https://github.com/dspinellis/manview'>manview</a>";
+    var parsedURI = URI.parse(window.location.href);
+    if (parsedURI.query) {
+      var query = URI.parseQuery(parsedURI.query);
+      if (query.name && query.link)
+	title += " — " + "<a href='" + query.link + "'>" + query.name + "</a>";
+      else if (query.name)
+	title += " — " + query.name;
+      else if (query.link)
+	title += " — " + "<a href='" + query.link + "'>Manual Page</a>";
+      this.el.innerHTML = title;
+    }
+  },
+
+});
