38c7f77a64ab4d7e63aa1b484d327bb9b84d1697
Diomidis Spinellis <dds@aueb.gr>
2019-10-12 19:53:38 +0000

    publish: Add development instructions
    generated from commit 41a5cf6ae1bb5c202c611a49251ac6e1fc38ba81

--- assets/build/js/scripts.js
+++ assets/build/js/scripts.js
@@ -329,8 +329,9 @@ var Lexer = function (source) {
 
 /**
  * Performs the following tasks to the source string:
- * - Replaces < and > symbols with their HTML escape equivalents
+ * - Replaces bullets, em-dashes, copyright with their HTML entities
  * - Adds whitespaces between escape sequences
+ * - Replaces < and > symbols with their HTML escape equivalents
  *
  * @argument {string} source
  *
@@ -341,6 +342,9 @@ var Lexer = function (source) {
  */
 Lexer.prototype.cleanSource = function (source) {
   return source
+    .replace(/\\\(bu|\\\[bu\]/g, '&bull;')
+    .replace(/\\\(em|\\\[em\]/g, '&mdash;')
+    .replace(/\\\(co|\\\[co\]/g, '&copy;')
     .replace(patterns.escape, ' $1 ')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;');
