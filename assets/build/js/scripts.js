d00aba5a9ba6ffef14240f23d24f0d47d0672a23
Diomidis Spinellis <dds@aueb.gr>
2026-05-31 12:53:09 +0300

    publish: Add debugging info
    generated from commit 9b2b28faf9f765efa8fc43aea2e8b8a390295846

--- assets/build/js/scripts.js
+++ assets/build/js/scripts.js
@@ -3949,171 +3949,29 @@ ManView.Behaviors.PageView = Essential.Behavior.extend({
     request.responseType = 'text';
     request.onload = function() {
       var etag = request.getResponseHeader('ETag');
+      console.log('Source status:', request.status, request.statusText);
+      console.log('Source final URL:', request.responseURL);
+      console.log('Source Last-Modified header:', request.getResponseHeader('Last-Modified'));
+      console.log('Source ETag header:', etag);
+      console.log('Source all response headers:\n' + request.getAllResponseHeaders());
+
       var dispatchSource = function(fingerprint) {
         var customEvent = new CustomEvent("source:retrieved", {
 	  "detail": {
 	    text: request.response,
 	    lastModified: request.getResponseHeader('Last-Modified'),
-	    etag: etag || fingerprint
+	    etag: fingerprint
 	  }
         });
 
+        console.log('Dispatched source detail:', customEvent.detail);
         document.dispatchEvent(customEvent);
         console.log("Received " + request.response.length + " bytes");
       };
 
-      if (etag) {
-        dispatchSource(etag);
-	return;
-      }
-
-      this.computeTextSha256(request.response)
-	.then(dispatchSource)
-	.catch(function() {
-	  dispatchSource(this.computeTextSha256Sync(request.response));
-	}.bind(this));
-    }.bind(this);
+      dispatchSource(etag);
+    };
     request.send();
-  },
-
-  computeTextSha256: function(text) {
-    var bytes;
-
-    if (!window.crypto || !window.crypto.subtle || !window.TextEncoder) {
-      return Promise.reject();
-    }
-
-    bytes = new TextEncoder().encode(text);
-
-    return window.crypto.subtle.digest('SHA-256', bytes)
-      .then(function(hash) {
-	return Array.prototype.map.call(new Uint8Array(hash), function(byte) {
-	  return ('00' + byte.toString(16)).slice(-2);
-	}).join('');
-      });
-  },
-
-  computeTextSha256Sync: function(text) {
-    var bytes = this.utf8Bytes(text);
-    var hash = this.sha256(bytes);
-
-    return hash.map(function(word) {
-      return ('00000000' + word.toString(16)).slice(-8);
-    }).join('');
-  },
-
-  utf8Bytes: function(text) {
-    var bytes = [];
-    var i;
-    var code;
-
-    for (i = 0; i < text.length; i++) {
-      code = text.charCodeAt(i);
-      if (code < 0x80) {
-	bytes.push(code);
-      } else if (code < 0x800) {
-	bytes.push(0xc0 | (code >> 6));
-	bytes.push(0x80 | (code & 0x3f));
-      } else if (code >= 0xd800 && code <= 0xdbff) {
-	i++;
-	code = 0x10000 + (((code & 0x3ff) << 10) | (text.charCodeAt(i) & 0x3ff));
-	bytes.push(0xf0 | (code >> 18));
-	bytes.push(0x80 | ((code >> 12) & 0x3f));
-	bytes.push(0x80 | ((code >> 6) & 0x3f));
-	bytes.push(0x80 | (code & 0x3f));
-      } else {
-	bytes.push(0xe0 | (code >> 12));
-	bytes.push(0x80 | ((code >> 6) & 0x3f));
-	bytes.push(0x80 | (code & 0x3f));
-      }
-    }
-
-    return bytes;
-  },
-
-  sha256: function(bytes) {
-    var h = [
-      0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
-      0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
-    ];
-    var k = [
-      0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-      0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-      0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-      0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-      0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-      0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-      0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-      0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
-    ];
-    var w = [];
-    var bitLength = bytes.length * 8;
-    var i;
-    var j;
-    var a;
-    var b;
-    var c;
-    var d;
-    var e;
-    var f;
-    var g;
-    var hh;
-    var s0;
-    var s1;
-    var ch;
-    var maj;
-    var temp1;
-    var temp2;
-
-    bytes.push(0x80);
-    while ((bytes.length % 64) !== 56) {
-      bytes.push(0);
-    }
-    for (i = 7; i >= 0; i--) {
-      bytes.push((bitLength / Math.pow(2, i * 8)) & 0xff);
-    }
-
-    for (i = 0; i < bytes.length; i += 64) {
-      for (j = 0; j < 16; j++) {
-	w[j] = ((bytes[i + j * 4] << 24) | (bytes[i + j * 4 + 1] << 16) |
-	  (bytes[i + j * 4 + 2] << 8) | bytes[i + j * 4 + 3]) >>> 0;
-      }
-      for (j = 16; j < 64; j++) {
-	s0 = (this.rotateRight(w[j - 15], 7) ^ this.rotateRight(w[j - 15], 18) ^ (w[j - 15] >>> 3)) >>> 0;
-	s1 = (this.rotateRight(w[j - 2], 17) ^ this.rotateRight(w[j - 2], 19) ^ (w[j - 2] >>> 10)) >>> 0;
-	w[j] = (w[j - 16] + s0 + w[j - 7] + s1) >>> 0;
-      }
-
-      a = h[0]; b = h[1]; c = h[2]; d = h[3];
-      e = h[4]; f = h[5]; g = h[6]; hh = h[7];
-
-      for (j = 0; j < 64; j++) {
-	s1 = (this.rotateRight(e, 6) ^ this.rotateRight(e, 11) ^ this.rotateRight(e, 25)) >>> 0;
-	ch = ((e & f) ^ ((~e) & g)) >>> 0;
-	temp1 = (hh + s1 + ch + k[j] + w[j]) >>> 0;
-	s0 = (this.rotateRight(a, 2) ^ this.rotateRight(a, 13) ^ this.rotateRight(a, 22)) >>> 0;
-	maj = ((a & b) ^ (a & c) ^ (b & c)) >>> 0;
-	temp2 = (s0 + maj) >>> 0;
-
-	hh = g; g = f; f = e; e = (d + temp1) >>> 0;
-	d = c; c = b; b = a; a = (temp1 + temp2) >>> 0;
-      }
-
-      h[0] = (h[0] + a) >>> 0;
-      h[1] = (h[1] + b) >>> 0;
-      h[2] = (h[2] + c) >>> 0;
-      h[3] = (h[3] + d) >>> 0;
-      h[4] = (h[4] + e) >>> 0;
-      h[5] = (h[5] + f) >>> 0;
-      h[6] = (h[6] + g) >>> 0;
-      h[7] = (h[7] + hh) >>> 0;
-    }
-
-    return h;
-  },
-
-  rotateRight: function(value, bits) {
-    return (value >>> bits) | (value << (32 - bits));
   },
 
   channels: {
