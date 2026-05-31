f2e02b1825604b1f61994792bf67977038e92d7f
Diomidis Spinellis <dds@aueb.gr>
2026-05-31 12:09:44 +0300

    publish: Further try
    generated from commit e6416514f54b33368c57c13be2309bc90299ec76

--- assets/build/js/scripts.js
+++ assets/build/js/scripts.js
@@ -3932,13 +3932,16 @@ ManView.Behaviors.PageView = Essential.Behavior.extend({
   init: function() {
     var url = 'https://raw.githubusercontent.com/dspinellis/manview/master/manview.3';
     var parsedURI = URI.parse(window.location.href);
+    var query = {};
     console.log('parsedURI ' + parsedURI);
     if (parsedURI.query) {
-      var query = URI.parseQuery(parsedURI.query);
+      query = URI.parseQuery(parsedURI.query);
       console.log('Query: ' + query);
       if (query.src)
 	url = query.src;
     }
+    this.sourceName = query.name || '';
+    this.sourceLink = query.link || '';
     this.parser = ManView.Services.TextParser.new();
     var request = new XMLHttpRequest();
     console.log('Fetching ' + url);
@@ -3947,7 +3950,9 @@ ManView.Behaviors.PageView = Essential.Behavior.extend({
     request.onload = function() {
       var customEvent = new CustomEvent("source:retrieved", {
 	"detail": {
-	  text: request.response
+	  text: request.response,
+	  lastModified: request.getResponseHeader('Last-Modified'),
+	  etag: request.getResponseHeader('ETag')
 	}
       });
       document.dispatchEvent(customEvent);
@@ -3962,11 +3967,122 @@ ManView.Behaviors.PageView = Essential.Behavior.extend({
 
   refreshChannel: function(e) {
     var text = e.detail.text;
+    var hasHeader = text.search(/^\.(TH|Dt)\b/m) != -1;
+    var html;
     console.log("Source text length: " + text.length);
     if (text.search(".Dd") != -1)
       this.parser.setMacroLib("doc");
-    this.el.innerHTML = this.parser.parseGroff(text);
+    html = this.parser.parseGroff(text);
+    this.renderManual(html, hasHeader);
+    this.setFooter(e.detail.lastModified, e.detail.etag);
     console.log("HTML length: " + this.el.innerHTML.length);
+  },
+
+  renderManual: function(html, hasHeader) {
+    var container = document.createElement('div');
+    var header;
+
+    container.innerHTML = html;
+    header = container.querySelector('p:first-of-type');
+
+    if (hasHeader && header) {
+      if (this.sourceLink) {
+	this.linkHeaderSpans(header);
+      }
+      this.setTopBar(header);
+      header.parentNode.removeChild(header);
+    } else if (this.sourceName) {
+      this.setTopBar(this.createFallbackHeader());
+    }
+
+    this.el.innerHTML = container.innerHTML;
+  },
+
+  setTopBar: function(header) {
+    var title = document.querySelector('[data-behavior="set-title"]');
+
+    if (title) {
+      title.innerHTML = header.innerHTML;
+    }
+  },
+
+  setFooter: function(lastModified, etag) {
+    var footerDate = document.querySelector('[data-role="manual-date"]');
+    var footerEtag = document.querySelector('[data-role="manual-etag"]');
+    var date = lastModified ? new Date(lastModified) : new Date();
+
+    if (isNaN(date.getTime())) {
+      date = new Date();
+    }
+
+    if (footerDate) {
+      footerDate.textContent = this.formatIsoDate(date);
+    }
+    if (footerEtag) {
+      footerEtag.textContent = etag ? '(' + this.cleanEtag(etag).slice(0, 6) + ')' : '';
+    }
+  },
+
+  cleanEtag: function(etag) {
+    return etag.replace(/^W\//, '').replace(/"/g, '');
+  },
+
+  formatIsoDate: function(date) {
+    var month = String(date.getMonth() + 1);
+    var day = String(date.getDate());
+
+    if (month.length < 2) {
+      month = '0' + month;
+    }
+    if (day.length < 2) {
+      day = '0' + day;
+    }
+
+    return date.getFullYear() + '-' + month + '-' + day;
+  },
+
+  linkHeaderSpans: function(header) {
+    var spans = header.querySelectorAll('span');
+    var i;
+
+    for (i = 0; i < spans.length; i++) {
+      this.wrapContentsWithLink(spans[i]);
+    }
+  },
+
+  wrapContentsWithLink: function(element) {
+    var link = document.createElement('a');
+
+    link.href = this.sourceLink;
+    while (element.firstChild) {
+      link.appendChild(element.firstChild);
+    }
+    element.appendChild(link);
+  },
+
+  createFallbackHeader: function() {
+    var header = document.createElement('p');
+    var span = document.createElement('span');
+
+    header.className = 'manual-header';
+
+    if (this.sourceLink) {
+      span.appendChild(this.createSourceLink(this.sourceName));
+    } else {
+      span.textContent = this.sourceName;
+    }
+
+    header.appendChild(span);
+    return header;
+  },
+
+  createSourceLink: function(text) {
+    var link = document.createElement('a');
+
+    link.href = this.sourceLink;
+    link.textContent = text;
+
+    return link;
   },
 
 });
@@ -3978,18 +4094,7 @@ ManView.Behaviors.SetTitle = Essential.Behavior.extend({
   priority: 1,
 
   init: function() {
-    var title = "<a href='https://github.com/dspinellis/manview'>manview</a>";
-    var parsedURI = URI.parse(window.location.href);
-    if (parsedURI.query) {
-      var query = URI.parseQuery(parsedURI.query);
-      if (query.name && query.link)
-	title += " — " + "<a href='" + query.link + "'>" + query.name + "</a>";
-      else if (query.name)
-	title += " — " + query.name;
-      else if (query.link)
-	title += " — " + "<a href='" + query.link + "'>Manual Page</a>";
-      this.el.innerHTML = title;
-    }
+    this.el.innerHTML = "<a href='https://github.com/dspinellis/manview'>manview</a>";
   },
 
 });
